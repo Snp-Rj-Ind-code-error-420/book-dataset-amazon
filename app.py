f82af8517116b7360d02383a7c44f98922d52981
Raja Sardar <80396878+Snp-Rj-Ind-code-error-420@users.noreply.github.com>
2025-04-13 17:46:29 +0530

    add exception toast

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
+try :
+	# x=1/0
+	pages=[
+		st.Page("pages/homepage.py",title="Overview",icon=":material/subject:"),
+		st.Page("pages/EDA.py",title="EDA(Exploratory Data Analysis)",icon=":material/data_exploration:"),
+		st.Page("pages/insights.py",title="Insights",icon=":material/search_insights:"),
+	]
 
-pages=[
-	st.Page("pages/homepage.py",title="Overview",icon=":material/subject:"),
-	st.Page("pages/EDA.py",title="EDA(Exploratory Data Analysis)",icon=":material/data_exploration:"),
-	st.Page("pages/insights.py",title="Insights",icon=":material/search_insights:"),
-]
 
 
-pg = st.navigation(pages)
-# uhouh/sda
-pg.run()
+
+	pg = st.navigation(pages)
+	# uhouh/sda
+	pg.run()
+except Exception as e:
+	print(f'an exception occure {e} try rebooting')
+	st.toast(f'an exception occure {e} try rebooting',icon="🚨")
